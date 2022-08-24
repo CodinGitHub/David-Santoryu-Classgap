f8cda6ddb20889c49fc68cd3e77a2aab7e9cd22d
CodinGitHub <gms2programador@gmail.com>
2022-08-24 14:04:42 -0500

    Listado de peliculas

--- objetos.js
+++ objetos.js
@@ -7,15 +7,19 @@ let user = {
     developer: true,
     'es miembro': false
 };
+// console.log(user)
 
-//METODOS DE OBJETOS
+// Para consultar si existe una propiedad en un objeto
+// console.log(user.hasOwnProperty('email'))
+
+// METODOS DE OBJETOS
 
 //Borrar
 delete user.name;
 
 //Consultar si existe un key/propiedad
-console.log(Object.keys(user));
-console.log(Object.values(user));
+// console.log(Object.keys(user));
+// console.log(Object.values(user));
 
 //CLASES
 class Users{
@@ -29,6 +33,49 @@ class Users{
 let user1 = new Users('Roberto', 25, true);
 let user2 = new Users('Maria', 31, false);
 
-console.log(user1)
-console.log(user2)
+/* ------------------------- */
 
+class Product{
+    constructor(img, producName, price, quantity){
+        this.img = img;
+        this.producName = producName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    subtotal(){
+        return this.price * this.quantity;
+    }
+}
+
+let producto1 = new Product('img/product1.jpg', 'B con logo', 18, 2);
+let producto2 = new Product('img/product2.jpg', 'B', 26, 3);
+let producto3 = new Product('img/product2.jpg', 'B', 10, 1);
+
+//AGREGAR PRODUCTOS AL CARRO
+
+let cart = [producto1, producto2, producto3];
+
+// let subtotal = 0;
+// for(let i=0; i<cart.length; i++){
+//     subtotal = subtotal + cart[i].subtotal()
+// }
+
+// console.log(subtotal)
+
+// forEach
+
+// let subtotal = 0
+// cart.forEach( product=>{
+//     subtotal = subtotal + product.subtotal()
+// } );
+
+// console.log(subtotal)
+
+// MAP - Siempre genera un arreglo, con el resultado de la funcion como elementos
+let subtotal = 0
+let resultado = cart.map(product => {
+    // return subtotal = subtotal + product.subtotal()
+    return product.subtotal()*2
+})
+
+console.log(resultado)
---
+++ peliculas.js
@@ -0,0 +1,16 @@
+import data from './movies.json' assert {type: 'json'}
+
+console.log(data);
+
+/* 
+David, en la variable data esta el listado de peliculas, es un arreglo de objetos. 
+Sobre él debes trabajar. Puedes mirar en consola que es el que se imprime, sugiero
+cambiar los filtros ya que es ligeramente diferete la informaciòn de estas peliculas:
+
+Filtrar por:
+
+Año -> Ejemplo peliculas que sean de 1980 en adelante
+Genero -> Peliculas que sean solo de 'Drama' por ejemplo
+
+Siente libre de agregar mas filtros si deseas
+*/
